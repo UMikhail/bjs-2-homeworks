1a1ad1e5542c0870fd7f20f0e1dea4f80b7b214a
Михаил Уткин <mixail.rasto4u@yandex.ru>
2022-11-02 20:08:04 +0400

    Добавление проекта

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,60 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = Infinity
+  max = -Infinity
+  sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]  //находим максимальное значение массива
+    }
+    if (arr[i] < min) {
+      min = arr[i]  //находим минимальное значение массива
+    }
+  }
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]  //находим сумму значений массива
+  }
+
+  avg = sum / arr.length //находим среднее значение массива
+  avg = Number(avg.toFixed(2)) //округляем получившееся значение до двух знаков после запятой и возвращаем это значение из string в number
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i] // считаем сумму элементов массива
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sumElemet = func(arrOfArr[i]) // включаем функцию для каждого элемента массива
+    if (sumElemet > max) {
+      max = sumElemet //ищем максимальное значение элемента
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let minMeaning = Infinity
+  let maxMeaning = -Infinity
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > maxMeaning) {
+      maxMeaning = arr[i]  //находим максимальное значение массива
+    }
+    if (arr[i] < minMeaning) {
+      minMeaning = arr[i]  //находим минимальное значение массива
+    }
+  }
+  let difference = Math.abs(maxMeaning - minMeaning) //находим разницу max и min, метод Math.abs() - вычисление модуля
+  return difference
 }

c5e6a33c276f1212bf83420749b0fb2cc81a0685
Михаил Уткин <mixail.rasto4u@yandex.ru>
2022-11-09 19:42:53 +0400

    Добавление проекта

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name
+  this.gender = gender
+  this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark] // добавить первую оценку 
+    } else {
+    this.marks.push(mark) // добавить вторую и последующие оценки в массив
+    }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if(this.marks === undefined){ 
+    this.marks = [...marks]
+    } else {
+    this.marks.push(...marks)
+    }
+}
+
+Student.prototype.getAverage = function() {
+  avg = 0
+  for (let i = 0; i <  this.marks.length; i++) {
+    avg += this.marks[i]; 
+  }
+  return avg / this.marks.length
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
